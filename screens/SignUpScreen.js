0c47f1050117a66b05c65d9ce4aee3bbf280d09b
CinthiaV97 <cinths.vl.97@gmail.com>
2020-05-13 22:48:05 -0600

    PRUEBA 1
    PRUEBA 1

---
+++ screens/SignUpScreen.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+import SignUp from '../components/SignUp'
+
+export default class SignUpScreen extends Component{
+    render(){
+        return(
+        <SignUp/>
+        );
+    }
+}
