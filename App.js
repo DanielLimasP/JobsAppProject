f443e95aa4c538dc2ee66021f136a319c8562e18
AleRuelas <aleruelas03@gmail.com>
2020-05-17 00:49:14 -0600

    Assignment of the navigation in the App function

--- App.js
+++ App.js
@@ -2,25 +2,8 @@ import React from 'react';
 import AppNavigation from './src/navigation/AppNavigation';
 
 
-<<<<<<< HEAD
-import LoginView from './screens/LoginViewScreen'
-import SignIn from './components/SignIn'
-import SignUp from './components/SignUp'
-import Map from './components/Map'
-import PostJob from './components/PostJob'
-
-export default class App extends Component<>{
-  render(){
-    return(
-      <View>
-        <PostJob/>
-      </View>
-    )
-  }
-=======
 function App(){
   return <AppNavigation/>
->>>>>>> feature
 }
 
 export default App;
