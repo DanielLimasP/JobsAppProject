4d0dfd07820dd39408857d1eca68c1a302df0611
AleRuelas <aleruelas03@gmail.com>
2020-05-17 00:09:48 -0600

    Organization of resource folders and connection of the Splash and Login screens.

--- App.js
+++ App.js
@@ -1,22 +1,9 @@
-import React, {
-  Component
-}  from 'react';
+import React from 'react';
+import AppNavigation from './src/navigation/AppNavigation';
 
-import {
-  View
-} from 'react-native';
 
-import LoginView from './screens/LoginViewScreen'
-import SignIn from './components/SignIn'
-
-export default class App extends Component<>{
-  render(){
-    return(
-      <View>
-        <SignIn/>
-      </View>
-    )
-  }
+function App(){
+  return <AppNavigation/>
 }
 
-
+export default App;
