ec2c4bc9a0d18fb312b14fb525c3e4bbcfe7392d
César Sáenz <42789030+saenzsalazar@users.noreply.github.com>
2020-05-21 22:44:29 -0600

    Tabs Implemetation

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,36 +1,66 @@
 
-import React, { useContext, useEffect } from 'react';
-import { View, Text, StyleSheet, Alert, BackHandler } from 'react-native';
-import { mainStyles } from '../styles/styles'
 
-import { UsuarioContext } from '../context/UsuarioContext'
+//import * as React from 'react';
+//import { Text, View } from 'react-native';
+import { mainStyles } from '../styles/styles'
 import MyButton from '../components/MyButton'
 import ReactMap from '../components/ReactMap'
+import React, { useContext, useEffect } from 'react';
+import { UsuarioContext } from '../context/UsuarioContext'
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { View, Text, StyleSheet, Alert, BackHandler } from 'react-native';
+
+function RecordScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Record</Text>
+    </View>
+  );
+}
+
+function JobsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Jobs</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 export default function MainLogin(props) {
-
   useBackButton(SignOff)
-
   const [login, loginAction] = useContext(UsuarioContext)
 
   return (
-    <View style={mainStyles.container}>
-      <ReactMap>
-      </ReactMap> 
-      <View style={styles.bottomContainer}>
-        <Text
-          style={styles.txtVw}>
-          Pantalla Principal{'\n'}Usuario: {'\n' + login.usuario.email}
-        </Text>
-        <MyButton
-          titulo='Cerrar sesion'
-          trasparent={true}
-          onPress={() => SignOff()}
-        />
-      </View>
-    </View>
-
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Record" component={RecordScreen} />
+        <Tab.Screen name="Map" component={MapScreen} defa/>
+        <Tab.Screen name="Jobs" component={JobsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
+
+  function MapScreen() {
+    return (
+      <View style={mainStyles.container}>
+        <ReactMap/> 
+        <View style={styles.bottomContainer}>
+          <Text
+            style={styles.txtVw}>
+            Pantalla Principal{'\n'}Usuario: {'\n' + login.usuario.email}
+          </Text>
+          <MyButton
+            titulo='Cerrar sesion'
+            trasparent={true}
+            onPress={() => SignOff()}
+          />
+        </View>
+      </View>
+    );
+  }
 
   function goToScreen(routeName) {
     props.navigation.navigate(routeName)
@@ -55,7 +85,6 @@ export default function MainLogin(props) {
         }
       ]
     )
-
   }
 }
 
