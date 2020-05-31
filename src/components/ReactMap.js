8a58eb2ed959bb7a729794d1c1b2b6a546957a4b
Daniel Limas <48876636+DanielLimasP@users.noreply.github.com>
2020-05-31 01:01:37 -0600

    Now we display every single job in the map

--- src/components/ReactMap.js
+++ src/components/ReactMap.js
@@ -52,54 +52,184 @@ class ReactMap extends React.Component {
   componentDidMount(){
     fetchJobs().then((fetchedJobs) => {
       
+      // This is what bad code looks like...
         var coordArray = [
           {
             latitude: 28.637521,
             longitude: -106.066234
           },
           {
-            //28.630926, -106.068015
             latitude: 28.630926,
             longitude: -106.068015
           },
           {
-            //28.630493, -106.074452
             latitude: 28.630493,
             longitude: -106.074452
           },
           {
-            //28.630493, -106.074452
-            latitude: 28.630493,
-            longitude: -106.074452
-          },
-          {
-            //28.634423, -106.075005
-            latitude: 28.634423,
-            longitude: -106.075005
-          },
-          {
-            //28.636080, -106.075633
             latitude: 28.636080,
             longitude: -106.075633
           },
           {
-            //28.637135, -106.070365
-            latitude: 28.637135,
+            latitude: 28.637135, 
             longitude: -106.070365
           },
           {
-            //28.637521, -106.066234
             latitude: 28.637521,
             longitude: -106.066234
           },
           {
-            //28.632996, -106.069099
-            latitude: 28.632914,
-            longitude: -106.069500
-          }
+            latitude: 28.637582,
+            longitude: -106.059840
+          },
+          {
+            latitude: 28.650947,
+            longitude: -106.086358
+          },
+          {
+            latitude: 28.654647,
+            longitude: -106.098364
+          },
+          {
+            latitude: 28.659280, 
+            longitude: -106.088041
+          },
+          {
+            latitude: 28.661088,
+            longitude: -106.091067
+          },
+          {
+            latitude: 28.662331, 
+            longitude: -106.095155
+          },
+          {
+            latitude: 28.666003,
+            longitude: -106.096132
+          },
+          {
+            latitude: 28.666078,
+            longitude: -106.099040
+          },
+          {
+            latitude: 28.665202,
+            longitude: -106.101540
+          },
+          {
+            latitude: 28.665371,
+            longitude: -106.105167
+          },
+          {
+            latitude: 28.665740, 
+            longitude: -106.098737
+          },
+          {
+            latitude: 28.667811,
+            longitude: -106.097042
+          },
+          {
+            latitude: 28.670193,
+            longitude: -106.094006
+          },
+          {
+            latitude: 28.672283,
+            longitude: -106.091968
+          },
+          {
+            latitude: 28.674006,
+            longitude: -106.092719
+          },
+          {
+            latitude: 28.673912, 
+            longitude: -106.095365
+          },
+          {
+            latitude: 28.677388,
+            longitude: -106.094163
+          },
+          {
+            latitude: 28.679299,
+            longitude: -106.095493
+          },
+          {
+            latitude: 28.681332, 
+            longitude: -106.097639
+          },
+          {
+            latitude: 28.689342,
+            longitude: -106.084584
+          },
+          {
+            latitude: 28.712293,
+            longitude: -106.094518
+          },
+          {
+            latitude: 28.719956,
+            longitude: -106.101585
+          },
+          {
+            latitude: 28.726823, 
+            longitude: -106.094745
+          },
+          {
+            latitude: 28.727606,
+            longitude: -106.105479
+          },
+          {
+            latitude: 28.734324,
+            longitude: -106.129130
+          },
+          {
+            latitude: 28.735133,
+            longitude: -106.133121
+          },
+          {
+            latitude: 28.734239, 
+            longitude: -106.151778
+          },
+          {
+            latitude: 28.734653,
+            longitude: -106.164803
+          },
+          {
+            latitude: 28.754844,
+            longitude: -106.127086
+          },
+          {
+            latitude: 28.577669,
+            longitude: -106.031694
+          },
+          {
+            latitude: 28.580208,
+            longitude: -106.046316
+          },
+          {
+            latitude: 28.588198,
+            longitude: -106.041794
+          },
+          {
+            latitude: 28.594280,
+            longitude: -106.042847
+          },
+          {
+            latitude: 28.606503,
+            longitude: -106.043187
+          },
+          {
+            latitude: 28.616300, 
+            longitude: -106.055374
+          },
+          {
+            latitude: 28.617088,
+            longitude: -106.077573
+          },
+          {
+            latitude: 28.617415, 
+            longitude: -106.088980
+          },
+
         ]
 
-        for(let i = 0; i < 9; i++){
+        for(let i = 0; i < fetchedJobs.totalJobs; i++){
           //console.log(fetchedJobs)
           console.log(fetchedJobs.jobs[i].name)
           this.setState({
@@ -109,22 +239,14 @@ class ReactMap extends React.Component {
                 coordinate: coordArray[i],
                 key: id++,
                 title: fetchedJobs.jobs[i].name,
-                description: fetchedJobs.jobs[i].description
+                description: fetchedJobs.jobs[i].description,
+                amount: fetchedJobs.jobs[i].amountPayment
                 //color: randomColor()
               },
             ],
-            dataSource: [
-              ...this.state.dataSource,
-              {
-                name: fetchedJobs.jobs[i].name,
-                description: fetchedJobs.jobs[i].description,
-                payment: fetchedJobs.jobs[i].amountPayment
-              }
-            ]
           });
         }
       }).catch(e => {console.log(e)})
-
   }
 
   // View render function
@@ -150,7 +272,21 @@ class ReactMap extends React.Component {
               description = {marker.description}
               image = {require('../assets/images/jobmarker.png')}
             >
-            
+            <Callout tooltip>
+              <View>
+                <View style = {styles.bubble}> 
+                <Text style = {styles.name}>{marker.title}</Text>
+                <Text>{marker.description}</Text>
+          <Text>Pago: {marker.amount}</Text>
+                  <Image
+                    style = {styles.image}
+                    source = {require('../assets/images/logo.png')}
+                  />
+                </View>
+                <View style = {styles.arrowBorder}/>
+                <View style = {styles.arrow}/>
+              </View>
+            </Callout>
           </Marker>
           ))}   
 
@@ -250,9 +386,9 @@ const styles = StyleSheet.create({
     fontFamily: "roboto-regular"
   }, 
   image: {
-    width: 80,
-    height: 60,
-    marginTop: 20,
+    width: 40,
+    height: 40,
+    marginTop: 10,
     alignSelf: 'center'
   }
 })  
